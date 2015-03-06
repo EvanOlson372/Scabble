9177a8cf47b6f4cd565da40481eaf49be01c9e08
lmevange <leo327-tes@hotmail.com>
2015-03-06 13:31:06 -0500

    more tests
    added addPieceTest 2-4

--- src/tests/BoardTests.java
+++ src/tests/BoardTests.java
@@ -18,4 +18,31 @@ public class BoardTests {
 	assertTrue(t1==t2);
 	
 	}
+	@Test
+	public void addPieceTest2(){
+	Board b= new Board() ;
+	Tile t1 =new Tile ('H',5);
+	b.addPiece(5,7, t1);
+	b.getPiece(5, 7);
+	Tile t2 = b.getPiece(5, 7);
+	assertTrue(t1==t2);
+}
+	@Test
+	public void addPieceTest3(){
+	Board b= new Board() ;
+	Tile t1 =new Tile ('Y',2);
+	b.addPiece(7,8, t1);
+	b.getPiece(7, 8);
+	Tile t2 = b.getPiece(7, 8);
+	assertTrue(t1==t2);
+}
+	@Test
+	public void addPieceTest4(){
+	Board b= new Board() ;
+	Tile t1 =new Tile ('A',1);
+	b.addPiece(12,13, t1);
+	b.getPiece(12, 13);
+	Tile t2 = b.getPiece(12, 13);
+	assertTrue(t1==t2);
+}
 }
