7623574950891c1da35f93b7e8e8b27d05413511
Evan Olson <evanolson372@gmail.com>
2015-03-06 13:49:31 -0500

    tests
    TESTS

--- src/tests/TilebagTests.java
+++ src/tests/TilebagTests.java
@@ -13,7 +13,20 @@ public class TilebagTests {
 	@Test public void test02(){valuetest(5, 240);}
 	@Test public void test03(){valuetest(2, 15);}
 	@Test public void test04(){valuetest(1, 145);}
-	
+	@Test public void test05() { letterstest('B', 12); }
+	@Test public void test06() { letterstest('C', 12); }
+	@Test public void test07() { letterstest('D', 12); }
+	@Test public void test08() { letterstest('E', 29); }
+	@Test public void test09() { letterstest('F', 12); }
+	@Test public void test10() { letterstest('G', 12); }
+	@Test public void test11() { letterstest('H', 12); }
+	@Test public void test12() { letterstest('I', 29); }
+	@Test public void test13() { letterstest('J', 12); }
+	@Test public void test14() { letterstest('K', 12); }
+	@Test public void test15() { letterstest('L', 12); }
+	@Test public void test16() { letterstest('M', 12); }
+	@Test public void test17() { letterstest('M', 12); }
+
 	
 	
 	
